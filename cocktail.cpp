bc07dc3863509f0f6d7190d99cbbdcbc62cd0873
ave-salve <58994336+ave-salve@users.noreply.github.com>
2023-04-08 12:36:16 +0200

    upload

---
+++ cocktail.cpp
@@ -0,0 +1,40 @@
+struct Ingrediant
+{
+public:
+    double alk; // prozent alkohol
+    double sweet;
+    double bitter;
+    double sour;
+    double max_amount_cL; // cL, 0 heisst unlimitiert
+    bool co2;
+    double viscosity;
+    double cost_permax;
+
+    // colour
+
+    Ingrediant();
+    ~Ingrediant();
+};
+Ingrediant::Ingrediant(/* args */)
+{
+}
+Ingrediant::~Ingrediant()
+{
+}
+
+void addIngrediant()
+{
+    // Sam does his magic
+}
+
+int main()
+{
+
+    // desired drink: colour, volume, alkohol
+
+    // random generator
+
+    // create name
+
+    return 0;
+}
