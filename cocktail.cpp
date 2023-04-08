62426f0e276eef49614a232db7cf73c414c5d9fd
ave-salve <58994336+ave-salve@users.noreply.github.com>
2023-04-08 14:31:03 +0200

    basic structure

--- cocktail.cpp
+++ cocktail.cpp
@@ -1,15 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <cmath>
+#include <fstream>
+
+
 struct Ingrediant
 {
 public:
+    // default: value between 0, 1
+
     double alk; // prozent alkohol
-    double sweet;   //Test Git
+    double sweet;
     double bitter;
     double sour;
-    double max_amount_cL; // cL, 0 heisst unlimitiert
-    bool co2;
     double viscosity;
-    double cost_permax; // HÖHÖ, ich hoff es hät funkitoniert?? mal luege
-
+    double max_amount_cL; // in cL, 0 heisst unlimitiert
+    bool co2; // 0 oder 1
+    double cost_permax; // in CHF
     // colour
 
     Ingrediant();
@@ -22,15 +30,50 @@ Ingrediant::~Ingrediant()
 {
 }
 
-void addIngrediant()
+using List = std::vector<Ingrediant>;
+
+
+
+Ingrediant read_input(){}
+
+//returns a Vector of possible ingrediants to add to the drink. Sorted from most to least fitting
+List sort_ingreds(List& ingrediants, List& drink, Ingrediant& input)
 {
     // Sam does his magic
 }
 
+// post: vector of probabilities (e^{-ax_i})
+std::vector<double> make_dist(int n_ingreds, int randomness){
+    // see notizblatt
+}
+
+List select_ingreds(List& ingrediants, Ingrediant& input){
+    while(input.max_amount_cL > 0){
+        // call sort_ingreds
+        // call make_dist
+        // radnom generator -> choose ingrediant
+        // subtract volume
+
+
+
+
+    }
+}
+
+std::string give_name(List drink){
+    return "bob";
+}
+
+void print_drink(List drink){}
+
+
+
+
+
 int main()
 {
-
     // desired drink: colour, volume, alkohol
+    Ingrediant input = read_input();
 
     // random generator
 
